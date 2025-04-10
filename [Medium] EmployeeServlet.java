efdf47a414e009055388042e4b14e2c876e746b3
techmo <65083936+Techmo29@users.noreply.github.com>
2025-04-10 16:02:57 +0530

    Create [Medium] EmployeeServlet.java

---
+++ [Medium] EmployeeServlet.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class EmployeeServlet extends HttpServlet {
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    String empId = request.getParameter("empId");
+
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "password");
+
+      Statement stmt = con.createStatement();
+      ResultSet rs;
+
+      if (empId != null && !empId.isEmpty()) {
+        rs = stmt.executeQuery("SELECT * FROM employees WHERE id = " + empId);
+      } else {
+        rs = stmt.executeQuery("SELECT * FROM employees");
+      }
+
+      out.println("<table border='1'><tr><th>ID</th><th>Name</th></tr>");
+      while (rs.next()) {
+        out.println("<tr><td>" + rs.getInt("id") + "</td><td>" + rs.getString("name") + "</td></tr>");
+      }
+      out.println("</table>");
+
+      con.close();
+    } catch (Exception e) {
+      out.println("Error: " + e.getMessage());
+    }
+  }
+}
